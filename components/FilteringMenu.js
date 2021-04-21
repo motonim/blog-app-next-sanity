6c909ce7370a9e5db3341a131e02889472d4d74e
Jaeri Park <motonimjrp@gmail.com>
2021-04-21 19:50:34 -0400

    adding filter view function

---
+++ components/FilteringMenu.js
@@ -0,0 +1,16 @@
+function FilteringMenu({ onChange }) {
+  return (
+    <div className='filtering-menu mb-2'>
+      <div
+        onClick={() => {
+          alert('clicking!');
+          onChange();
+        }}
+      >
+        Change View
+      </div>
+    </div>
+  );
+}
+
+export default FilteringMenu;
