ca38a4d3ea9d8e2ef212a01a001849e70691068e
Jaeri Park <motonimjrp@gmail.com>
2021-04-21 18:35:03 -0400

    adding image position to content image

--- components/BlogContent.js
+++ components/BlogContent.js
@@ -12,9 +12,20 @@ const serializers = {
         </HighlightCode>
       );
     },
-    image: ({ node: { asset, alt, position } }) => {
+    image: ({ node: { asset, alt, position = 'center' } }) => {
+      let style = {};
+
+      if (position === 'left') {
+        style.float = position;
+        style.marginRight = '30px';
+      }
+      if (position === 'right') {
+        style.float = position;
+        style.marginLeft = '30px';
+      }
+
       return (
-        <div className='blog-image'>
+        <div className='blog-image' style={{ ...style }}>
           <img src={urlFor(asset).height(500).fit('max').url()} />
           <div className='image-alt'>{alt}</div>
         </div>
