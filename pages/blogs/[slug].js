fea81fea9e32003c445090ded7b5ccf1f669bc3c
Jaeri Park <motonimjrp@gmail.com>
2021-04-24 22:50:15 -0400

    getting preview to page

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -8,8 +8,9 @@ import moment from 'moment';
 import { useRouter } from 'next/router';
 
 import BlogContent from 'components/BlogContent';
+import PreviewAlert from 'components/PreviewAlert';
 
-const BlogDetail = ({ blog }) => {
+const BlogDetail = ({ blog, preview }) => {
   const router = useRouter();
 
   if (!router.isFallback && !blog?.slug) {
@@ -24,6 +25,7 @@ const BlogDetail = ({ blog }) => {
     <PageLayout className='blog-detail-page'>
       <Row>
         <Col md={{ span: 10, offset: 1 }}>
+          {preview && <PreviewAlert />}
           <BlogHeader
             title={blog.title}
             subtitle={blog.subtitle}
@@ -39,11 +41,11 @@ const BlogDetail = ({ blog }) => {
   );
 };
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps({ params, preview = false, previewData }) {
   const blog = await getBlogBySlug(params.slug);
 
   return {
-    props: { blog },
+    props: { blog, preview },
   };
 }
 
--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,9 @@ import FilteringMenu from 'components/FilteringMenu';
 
 import useGetBlogsPages from 'actions/pagination';
 import { getPaginatedBlogs } from 'lib/api';
+import PreviewAlert from 'components/PreviewAlert';
 
-export default function Home({ blogs }) {
+export default function Home({ blogs, preview }) {
   const [filter, setFilter] = useState({
     view: { list: 0 },
     date: { asc: 0 },
@@ -25,6 +26,7 @@ export default function Home({ blogs }) {
 
   return (
     <PageLayout>
+      {preview && <PreviewAlert />}
       <AuthorIntro />
       <FilteringMenu
         filter={filter}
@@ -50,11 +52,12 @@ export default function Home({ blogs }) {
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps({ preview = false }) {
   const blogs = await getPaginatedBlogs({ offset: 0, date: 'desc' });
   return {
     props: {
       blogs,
+      preview,
     },
   };
 }
